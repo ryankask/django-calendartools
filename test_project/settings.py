2f407fb5ef5377d00524cac8032c06e428c22295
Ryan Kaskel <dev@ryankaskel.com>
2012-09-21 13:44:49 +0100

    Update test settings for Django 1.5 compatiblity.

--- test_project/settings.py
+++ test_project/settings.py
@@ -1,5 +1,6 @@
 # Minimal settings used for testing.
 from os import path
+
 CURRENT_DIR = path.abspath(path.dirname(__file__))
 CALENDARTOOLS_DIR = path.abspath(path.dirname(path.dirname(__file__)))
 
@@ -8,15 +9,18 @@ DEBUG = TEMPLATE_DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'tests.db',
     },
 }
-MIDDLEWARE_CLASSES = [
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'threaded_multihost.middleware.ThreadLocalMiddleware',
-]
+)
 
 STATIC_ROOT      = ''
 STATICFILES_DIRS = (path.join(CURRENT_DIR, 'static'),)
@@ -45,19 +49,20 @@ INSTALLED_APPS = (
 )
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 TEMPLATE_DIRS = [path.join(CALENDARTOOLS_DIR, 'templates'),
                  path.join(CURRENT_DIR, 'templates')]
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
-    'django.core.context_processors.request',
-    "django.core.context_processors.static",
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
     'calendartools.context_processors.current_datetime',
     'calendartools.context_processors.current_site',
 )
