8170f0b4807f5ff77ccb925151ceaafdf632ee78
Ryan Kaskel <dev@ryankaskel.com>
2012-09-27 14:19:47 +0100

    More flexible testing command.

--- runtests.py
+++ runtests.py
@@ -9,7 +9,9 @@ def main():
     current_path = path.abspath(path.dirname(__file__))
     sys.path.insert(0, path.join(current_path, 'test_project'))
     os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
-    call_command('test', 'event')
+
+    labels = sys.argv[1:] if len(sys.argv) > 1 else ('event',)
+    call_command('test', *labels)
 
 if __name__ == '__main__':
     main()
