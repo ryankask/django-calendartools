f33d862654b72704a60319cb81b99012e253bff3
Ryan Kaskel <dev@ryankaskel.com>
2013-06-20 16:11:26 +0100

    Use namespace if available.

--- calendartools/decorators.py
+++ calendartools/decorators.py
@@ -1,14 +1,25 @@
 from functools import wraps
 
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, resolve, Resolver404
 from django.http import HttpResponseRedirect
+
 
 def get_occurrence_data_from_session(view):
     @wraps(view)
     def new_view(request, *args, **kwargs):
+        try:
+            match = resolve(request.path)
+        except Resolver404:
+            url = '/'
+        else:
+            if match.namespace:
+                url = reverse('{}:event-list'.format(match.namespace))
+            else:
+                url = reverse('event-list')
+
         occurrence_info = request.session.get('occurrence_info')
         if not occurrence_info:
-            return HttpResponseRedirect(reverse('event-list'))
+            return HttpResponseRedirect(url)
 
         event               = occurrence_info['event']
         valid_occurrences   = occurrence_info['valid_occurrences']
